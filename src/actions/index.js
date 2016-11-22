8ccab1db99903300f3815c48ca494cc2628a7dd2
mat <reallymathew@gmail.com>
2016-11-22 11:54:08 -0600

    move fetch to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,32 @@ export function setSignIn(bool) {
         payload: bool,
     };
 }
+
+fetch('https://polar-escarpment-86427.herokuapp.com/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'tester123',
+        password: 'tester123'
+      })
+    }).then(response => response.json().then(json => ({ json, response })))
+      .then(({json, response}) => {
+      if (response.ok === false) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+    .then(
+      data => {
+    console.log(data);
+      },
+      ({response, data}) => {
+          console.log(data);
+          
+          if(response.status == 401) {
+              console.log(data.error);
+          }
+      }
+    );
